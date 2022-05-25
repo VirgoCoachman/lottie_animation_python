486ec34a2d9a7286a99b34dd2aa12126cad4d153
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-05-26 00:07:07 +0200

    Improving the lottie animation (follow_path) Add calculate_distance function to return the shortest distance between 2 points
    Add get_points_arouond_vertex function to return 2 auxiliary point around the vertex
    
    Add nearest_point function to return the point whom cordinates place it closer (less or equal to 5) the main vertex
    
    Add add_points_to_bezier function to populate the bezier with points needed in the animation.
    
    Disable the reverse mode in follow_path() method
    
    Implement CONSTANT DESIGN PATTERN

--- tornado_server.py
+++ tornado_server.py
@@ -1,25 +1,21 @@
 #!./venv/bin/python3
-import sys
+import math
 import os
 
-from lottie.utils import script
-from lottie import objects
-from lottie.utils.animation import follow_path
-from lottie.utils import animation as anutils
-from lottie import Point, Color
-
 import tornado.web
-from tornado import httputil
-from tornado.escape import json_decode
+from lottie import Point, Color
+from lottie import objects
+from lottie.utils import animation as anutils
+from lottie.utils import script
+from lottie.utils.animation import follow_path
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler, url
 
 points = [
     (80, 40),
     (95, 100),
-    (108, 100),
     (230, 100),
-    (290, 245),
+    (230, 245),
     (400, 245),
     (300, 313),
     (423, 412),
@@ -28,7 +24,52 @@ points = [
     (380, 80),
 ]
 
-DUREE = 180
+DUREE = 200
+MARGE_COIN = 2
+INDEX_HTML = "index.html"
+
+
+def calculate_distance(a: Point, b: Point):
+    return math.sqrt((b[0] - a[0]) ** 2 + (b[1] -a[1]) ** 2)
+
+
+def nearest_point(a: Point, vertex: Point) -> Point:
+    distance = calculate_distance(a, vertex)
+    middle = Point((a[0] + vertex[0]) / 2, (a[1] + vertex[1]) / 2)
+
+    if distance <= MARGE_COIN:
+        return middle
+
+    else:
+        return nearest_point(middle, vertex)
+
+def get_points_around_vertex(a: Point, vertex: Point, c: Point):
+    a1 = nearest_point(a, vertex)
+    b1 = nearest_point(c, vertex)
+    # points.append(a1,b1)
+
+    print("#"*20)
+    print(a1)
+    print(vertex)
+    print(b1)
+    print("#" * 20)
+    return a1, b1
+
+def add_points_to_bezier(bez, les_points):
+    size = len(les_points) - 2
+    previous = None
+    for i in range(size + 1):
+        if i == 0:
+            bez.shape.value.add_point(Point(les_points[i][0], les_points[i][1]))
+            previous = les_points[i]
+
+        else:
+            before_point, after_point = get_points_around_vertex(previous, les_points[i], les_points[i + 1])
+
+            bez.shape.value.add_point(Point(before_point.x, before_point.y))
+            bez.shape.value.add_point(Point(les_points[i][0], les_points[i][1]))
+            bez.shape.value.add_point(Point(after_point.x, after_point.y))
+    bez.shape.value.add_point(Point(les_points[size + 1][0], les_points[size + 1][1]))
 
 
 class FollowPathHandler(RequestHandler):
@@ -38,8 +79,7 @@ class FollowPathHandler(RequestHandler):
         an.assets.append(background_image)
 
         layer = objects.ShapeLayer()
-        # layer = objects.ImageLayer(background_image.id)
-        
+
         an.add_layer(layer)
         an.add_layer(objects.ImageLayer(background_image.id))
 
@@ -48,33 +88,30 @@ class FollowPathHandler(RequestHandler):
         ball.size.value = Point(20, 20)
         
         les_points = list(points)
-        sequence = 3 * int(DUREE / len(les_points) - 1)
 
         group.add_shape(objects.Fill(Color(1, 0, 0)))
 
         group = layer.add_shape(objects.Group())
         bez = group.add_shape(objects.Path())
-        
-        while len(les_points) > 0:
-            point = les_points.pop()
-            bez.shape.value.add_point(Point(point[0], point[1]))
-            
 
-        # sh = anutils.generate_path_segment(bez.shape.value, 0, 180, 60, 180, 60, True)
-        # group.add_shape(sh)
+        add_points_to_bezier(bez, les_points)
+
         group.add_shape(objects.Stroke(Color(0, 1, 0), 10))
-        
+
+        sequence = 3 * int(DUREE / len(les_points) - 1)
         i = 0 - sequence
         while i < DUREE:
             i = i + sequence
-            follow_path(ball.position, bez.shape.value, 0, 60, 30, True, Point(0, 0))
-            follow_path(ball.position, bez.shape.value, 60, 120, 30, True, Point(0, 0))
-            follow_path(ball.position, bez.shape.value, 120, 180, 30, True, Point(0, 0))
+            follow_path(ball.position, bez.shape.value, 0, 60, 30, False, Point(0, 0))
+            follow_path(ball.position, bez.shape.value, 60, 120, 30, False, Point(0, 0))
+            follow_path(ball.position, bez.shape.value, 120, 180, 30, False, Point(0, 0))
 
         # print(an.to_dict())
         script.script_main(an, path="./static/", basename=art, formats=["json"])
         
-        self.render("index.html")
+        self.render(INDEX_HTML)
+
+
 
 
 class ImageHandler(RequestHandler):
@@ -85,35 +122,33 @@ class ImageHandler(RequestHandler):
         an.assets.append(background_image)
 
         layer = objects.ShapeLayer()
-        # layer = objects.ImageLayer(background_image.id)
-        
+
         an.add_layer(layer)
         an.add_layer(objects.ImageLayer(background_image.id))
 
         group = layer.add_shape(objects.Group())
         bez = group.add_shape(objects.Path())
         les_points = list(points)
-        while len(les_points) > 0:
-            point = les_points.pop()
-            bez.shape.value.add_point(Point(point[0], point[1]))
+
+        add_points_to_bezier(bez, les_points)
 
         group.add_shape(objects.Stroke(Color(1, 0, 0), 10))
 
 
         group = layer.add_shape(objects.Group())
-        sh = anutils.generate_path_segment(bez.shape.value, 0, 180, 60, 180, 60, True)
+        sh = anutils.generate_path_segment(bez.shape.value, 0, 180, 60, 180, 30, False)
         group.add_shape(sh)
         group.add_shape(objects.Stroke(Color(0, 1, 0), 20))
 
         # print(an.to_dict())
         script.script_main(an, path="./static/", basename=art, formats=["json"])
         
-        self.render("index.html")
+        self.render(INDEX_HTML)
         
         
 class LottiePlayerHandler(RequestHandler):
     def get(self):
-        self.render("index.html")
+        self.render(INDEX_HTML)
     
         
 class App(Application):
